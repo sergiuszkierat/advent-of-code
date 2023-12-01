403bb2dee8019b61ac8ebd97e62048328f420c2a
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2023-12-01 18:55:20 +0100

    wip

--- src/2023/day01/Trebuchet.scala
+++ src/2023/day01/Trebuchet.scala
@@ -1,2 +1,16 @@
+
+
 @main
-def hello() = println("Hello, world!")
+def trebuchet() =
+  val lines = os.read.lines(os.pwd / os.up / "src" / "2023" / "day01" / "small.txt")
+  println(s"Calibration of line : ${lines.take(1).size}")
+
+
+def calibration(line: String) = 0
+
+
+
+//1abc2
+//pqr3stu8vwx
+//a1b2c3d4e5f
+//treb7uchet
