7071f2c95242c38b28d0df1525a4fadb2b5bd382
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2023-12-02 17:33:04 +0100

    day1 refactoring

--- src/2023/day01/Trebuchet.scala
+++ src/2023/day01/Trebuchet.scala
@@ -3,49 +3,36 @@ import scala.util.matching.Regex
 @main
 def trebuchet(): Int =
   val lines = os.read.lines(os.pwd / os.up / "src" / "2023" / "day01" / "input.txt")
-  val result = part1(lines)
-  println(s"Part1 : $result")
-  result
+  val result1 = part1(lines)
+  println(s"Part1 : $result1")
+  val result2 = part2(lines)
+  println(s"Part2 : $result2")
+  result2
 
-def part1(lines: Seq[String]): Int = lines.map(calibration).sum
+val firstNumber1: Regex = "([1-9])".r
+val firstNumber2: Regex = "([1-9]|one|two|three|four|five|six|seven|eight|nine)".r
+val lastNumber1: Regex = "([1-9])".r
+val lastNumber2: Regex = "([1-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)".r
 
-val firstNumberPattern: Regex = "([1-9]|one|two|three|four|five|six|seven|eight|nine)".r
-val lastNumberPattern: Regex = "([1-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)".r
+def part1(lines: Seq[String]): Int = lines.map(calibration(_, firstNumber1, lastNumber1)).sum
+def part2(lines: Seq[String]): Int = lines.map(calibration(_, firstNumber2, lastNumber2)).sum
 
-def calibration(line: String): Int =
-  val firstNumber = firstNumberPattern.findFirstMatchIn(line)
-  val lastNumber = lastNumberPattern.findFirstMatchIn(line.reverse)
-  val first = firstNumber.map(_.group(0)).flatMap(numbers.get).getOrElse(0)
-  val last = lastNumber.map(_.group(0)).flatMap(numbers.get).getOrElse(0)
-  println(s"$line: $first$last")
+def calibration(line: String, firstPattern: Regex, lastPattern: Regex): Int =
+  val firstNumber = firstPattern.findFirstMatchIn(line)
+  val lastNumber = lastPattern.findFirstMatchIn(line.reverse)
+  val first = firstNumber.map(_.group(0)).map(number).getOrElse(0)
+  val last = lastNumber.map(_.group(0)).map(number).getOrElse(0)
   s"$first$last".toInt
 
-val numbers = Map(
-  "one" -> 1,
-  "eno" -> 1,
-  "two" -> 2,
-  "owt" -> 2,
-  "three" -> 3,
-  "eerht" -> 3,
-  "four" -> 4,
-  "ruof" -> 4,
-  "five" -> 5,
-  "evif" -> 5,
-  "six" -> 6,
-  "xis" -> 6,
-  "seven" -> 7,
-  "neves" -> 7,
-  "eight" -> 8,
-  "thgie" -> 8,
-  "nine" -> 9,
-  "enin" -> 9,
-  "1" -> 1,
-  "2" -> 2,
-  "3" -> 3,
-  "4" -> 4,
-  "5" -> 5,
-  "6" -> 6,
-  "7" -> 7,
-  "8" -> 8,
-  "9" -> 9
-)
+def number(v: String): Int = 
+  v match {
+    case "one" | "eno" | "1"  => 1
+    case "two" | "owt" | "2" => 2
+    case "three" | "eerht" | "3" => 3
+    case "four" | "ruof" | "4" => 4
+    case "five" | "evif" | "5" => 5
+    case "six" | "xis" | "6" => 6
+    case "seven" | "neves" | "7" => 7
+    case "eight" | "thgie" | "8" => 8
+    case "nine" |"enin" | "9" => 9
+  }
