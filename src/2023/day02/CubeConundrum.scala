db725db6ea9e055ec4320ef11171f817970041bf
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2023-12-03 14:52:52 +0100

    refactoring

--- src/2023/day02/CubeConundrum.scala
+++ src/2023/day02/CubeConundrum.scala
@@ -5,28 +5,34 @@ def cubeConundrum(): Unit =
   val lines = os.read.lines(os.pwd / os.up / "src" / "2023" / "day02" / "input.txt")
   val result1 = part1(lines)
   println(s"Part1 : $result1")
-//  val result2 = part2(lines)
-//  println(s"Part2 : $result2")
-//  result2
+  val result2 = part2(lines)
+  println(s"Part2 : $result2")
 
 type GameNo = Int
 
 case class Cube(howMany: Int, color: Color)
+
+case class Game(number: GameNo, cubes: Seq[Seq[Cube]])
 
 enum Color(val max: Int):
   case Red   extends Color(12)
   case Green   extends Color(13)
   case Blue   extends Color(14)
 
-def part1(lines: Seq[String]) = lines.flatMap(game).sum
-//def part2(lines: Seq[String]): Int = lines.map(calibration(_, firstNumber2, lastNumber2)).sum
+def part1(lines: Seq[String]) =
+  lines.flatMap { line =>
+    val game = parseGame(line)
+    val possible = game.cubes.map(isPossible).forall(_ == true)
+    Option.when(possible)(game.number)
+  }.sum
 
-def game(line: String): Option[GameNo] =
+def part2(lines: Seq[String]) = lines.map(calibration(_, firstNumber2, lastNumber2)).sum
+
+def parseGame(line: String): Game =
   val (gameNo, gameSets) = line match {
     case s"Game $number: $sets" => (number.toInt, sets)
   }
-  val possible = gameSets.split("; ").map(cubes).map(isPossible).forall(_ == true)
-  Option.when(possible)(gameNo)
+  Game(gameNo, gameSets.split("; ").map(cubes))
 
 def cubes(setOfCubes: String): Seq[Cube] =
   setOfCubes.split(", ").map {
